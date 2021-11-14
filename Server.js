9d31dbf3df2a018d1c24d0c73cf1eb091e27a030
Davi Fernando Rosa <davir0514@gmail.com>
2021-11-14 12:55:24 -0300

    Arquivo

---
+++ Server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const routes =  require('./routes');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+//CONEXAO COM BANCO DE DADOS MONGO DB
+mongoose.connect('mongodb://localhost:27017/Teste',{
+    useUnifiedTopology:true,
+    useNewUrlParser:true,
+   
+},function (err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log('MongoDB CONECTADO com sucesso!')
+    }
+});
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(port,function(){
+    console.log(`Servidor rodando na porta: ${port}`)
+});
